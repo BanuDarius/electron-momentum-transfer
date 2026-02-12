Fixed unit normalization

auto_compute.py
@@ -29,17 +29,19 @@ min_a0 = 0.02
 max_a0 = 0.60
 
 tf = 1e4
-core_num = 8
+zetax = 1.0
+zetay = 0.0
+core_num = 4
 omega = 0.057
-xif = 4.0 * pi
-num_phase = 256
+xif = 0.0 * pi
+num_phase = 512
 psi = -80.0 * pi
 steps_pond = 256
 num_full = 16000
 wave_count = 1.0
 sigma = 4.0 * pi
 square_size = 1.0
-sweep_steps = 256
+sweep_steps = 128
 steps_electromag = 4096
 phi = 90.0 * deg_to_rad
 theta = 90.0 * deg_to_rad
@@ -57,10 +59,10 @@ if __name__ == "__main__":
         a0 = min_a0 + (max_a0 - min_a0) * i / sweep_steps
         a0_array = np.append(a0_array, a0)
         
-        laser_1 = sim_init.LaserParameters(a0, sigma, omega, xif, 0.0, 1.0, phi, theta, psi, pond_integrate_steps)
-        laser_2 = sim_init.LaserParameters(a0, sigma, omega, xif, 0.0, 1.0, phi, -theta, psi, pond_integrate_steps)
-        laser_3 = sim_init.LaserParameters(a0, sigma, omega, xif, 0.0, 1.0, phi, -135.0 * deg_to_rad, psi, pond_integrate_steps)
-        laser_4 = sim_init.LaserParameters(a0, sigma, omega, xif, 0.0, 1.0, phi, 135.0 * deg_to_rad, psi, pond_integrate_steps)
+        laser_1 = sim_init.LaserParameters(a0, sigma, omega, xif, zetax, zetay, phi, theta, psi, pond_integrate_steps)
+        laser_2 = sim_init.LaserParameters(a0, sigma, omega, xif, zetax, zetay, phi, -theta, psi, pond_integrate_steps)
+        laser_3 = sim_init.LaserParameters(a0, sigma, omega, xif, zetax, zetay, phi, -135.0 * deg_to_rad, psi, pond_integrate_steps)
+        laser_4 = sim_init.LaserParameters(a0, sigma, omega, xif, zetax, zetay, phi, 135.0 * deg_to_rad, psi, pond_integrate_steps)
         
         lasers = (laser_1, laser_2, laser_3, laser_4)
         
@@ -94,7 +96,7 @@ if __name__ == "__main__":
         programs.find_max_p("electromagnetic", sim_parameters, z_axis)
         programs.find_enter_exit_time("electromagnetic", sim_parameters, y_axis, z_axis)
         
-        plots.plot_time_momentum("electromagnetic", sim_parameters, a0_array, y_axis, y_axis)
+        #plots.plot_time_momentum("electromagnetic", sim_parameters, a0_array, y_axis, y_axis)
         #plots.plot_enter_exit_time("electromagnetic", sim_parameters, a0_array, y_axis, y_axis)
         #plots.plot_phases("electromagnetic", sim_parameters, a0_array, y_axis, y_axis)
         