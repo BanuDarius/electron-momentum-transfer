e100439a7fd95cd22cc48abcab4956c0081ea77d
Darius <mateidariusbanu@gmail.com>
2026-03-06 20:52:25 +0200

    Updated slurm script binding

--- auto_slurm.sh
+++ auto_slurm.sh
@@ -4,9 +4,14 @@
 #SBATCH --error=errors-%j.txt
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=256
+#SBATCH --cpus-per-task=128
 #SBATCH --mem=4G
+#SBATCH --hint=nomultithread
 #SBATCH --time=2:00:00
+
+export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
+export OMP_PLACES=cores
+export OMP_PROC_BIND=true
 
 source $HOME/python-env/bin/activate
 
